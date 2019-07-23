71fa3a730f2f236e081baf24704afccb5d8d2c42
Erik Butcher <with_a_k@mac.com>
2019-07-23 14:58:03 -0600

    More error tests for ScheduleMatcher

--- src/library/ScheduleMatcher.js
+++ src/library/ScheduleMatcher.js
@@ -3,12 +3,12 @@ const support = require('./supportfunctions');
 
 function match(schedules, minimumAttendance) {
   let maximumAttendance = schedules.length;
-  
+
   if (maximumAttendance < 2) {
     throw new Error("Cannot match with less than two schedules. Try adding more schedules.")
   }
   if (maximumAttendance < minimumAttendance) {
-    throw new Error(`No options with ${minimumAttendance} schedules overlapping. Try adding more schedules.`);
+    throw new Error(`No options with ${minimumAttendance} schedules overlapping. Try adding more schedules or reducing your minimum attendance.`);
   }
   if (minimumAttendance < 2) {
     throw new Error("Cannot match less than two schedules. Try increasing your minimum attendance.");
--- src/library/ScheduleMatcher.test.js
+++ src/library/ScheduleMatcher.test.js
@@ -9,3 +9,16 @@ test('error with less than two schedules given', () => {
     ScheduleMatcher.match([], 2);
   }).toThrow('Cannot match with less than two schedules. Try adding more schedules.');
 });
+
+test('error with less than two schedules given', () => {
+  let minAtt = Math.floor(Math.random() * 5) + 3;
+  expect(() => {
+    ScheduleMatcher.match(["dummy", "dummy"], minAtt);
+  }).toThrow(`No options with ${minAtt} schedules overlapping. Try adding more schedules or reducing your minimum attendance.`);
+});
+
+test('error with too low minimum attendance', () => {
+  expect(() => {
+    ScheduleMatcher.match(["dummy", "dummy"], 1);
+  }).toThrow('Cannot match less than two schedules. Try increasing your minimum attendance.');
+});
